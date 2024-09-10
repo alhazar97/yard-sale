7c227cfa1c301ff3c3522528ea6d6d12b4c87a53
alhazar97 <escribame1@gmail.com>
2024-09-10 01:25:09 -0500

    fusion del menu carrito de compras y toogle

--- main.js
+++ main.js
@@ -4,13 +4,36 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const menuCarrito = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuBurger.addEventListener('click', toggleMobileMenu);
+menuCarrito.addEventListener('click', toggleMenuCarrito)
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+     desktopMenu.classList.toggle('inactive'); 
 }
 
 function toggleMobileMenu() {
+const isAsideClosed = aside.classList.contains('inactive');
+
+if(!isAsideClosed){
+    aside.classList.add('inactive');
+}
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMenuCarrito() {
+const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+if(!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive');
+}
+    aside.classList.toggle('inactive');
 }
