4f086163f9ad349db56110ba43a1072e2b864a71
alhazar97 <escribame1@gmail.com>
2024-09-24 00:39:14 -0500

    detalles de un producto nuevos id

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const menuBurger = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 const menuCarrito = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 const cardsContainer = document.querySelector('.cards-container')
 
@@ -14,19 +14,19 @@ menuBurger.addEventListener('click', toggleMobileMenu);
 menuCarrito.addEventListener('click', toggleMenuCarrito)
 
 function toggleDesktopMenu() {
-const isAsideClosed = aside.classList.contains('inactive');
+const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if(!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
      desktopMenu.classList.toggle('inactive'); 
 }
 
 function toggleMobileMenu() {
-const isAsideClosed = aside.classList.contains('inactive');
+const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
 if(!isAsideClosed){
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
 }
     mobileMenu.classList.toggle('inactive');
 }
@@ -37,7 +37,7 @@ const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 if(!isMobileMenuClosed){
     mobileMenu.classList.add('inactive');
 }
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
